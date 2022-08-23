5f51650140e7eda1d2f2d07202a582de54115a5b
Abir <abiruzzaman.molla@gmail.com>
2022-08-23 10:42:53 +0600

    feat: crud api

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $conn->connect_error ? die('Could not connect to database') : '';
 
 $res = array('error' => false);
 
-$action =  isset($_GET['action']) ? $_GET['action'] : 'read';;
+$action =  isset($_GET['action']) ? $_GET['action'] : 'read';
 
 if($action == 'read'){
 	$result = $conn->query("SELECT * FROM `users`");
@@ -19,54 +19,61 @@ if($action == 'read'){
 	$res['users'] = $users;
 }
 
-// if($action == 'create'){
-// 	$username = $_POST['username'];
-// 	$email = $_POST['email'];
-// 	$mobile = $_POST['mobile'];
+if($action == 'create'){
+	$username = test_input($_POST['username']);
+	$email = test_input($_POST['email']);
+	$mobile = test_input($_POST['mobile']);
 
-// 	$result = $conn->query("INSERT INTO `users` (`username`, `email`, `mobile`) VALUES ('$username', '$email', '$mobile') ");
+	$result = $conn->query("INSERT INTO `users` (`username`, `email`, `mobile`) VALUES ('$username', '$email', '$mobile') ");
 
-// 	if($result){
-// 		$res['message'] = "User added successfully";
-// 	} else {
-// 		$res['error'] = true;
-// 		$res['message'] = "Could not insert user";
-// 	}
-// }
+	if($result){
+		$res['message'] = "User added successfully";
+	} else {
+		$res['error'] = true;
+		$res['message'] = "Could not insert user";
+	}
+}
 
-// if($action == 'update'){
-// 	$id = $_POST['id'];
-// 	$username = $_POST['username'];
-// 	$email = $_POST['email'];
-// 	$mobile = $_POST['mobile'];
+if($action == 'update'){
+	$id = test_input($_POST['id']);
+	$username = test_input($_POST['username']);
+	$email = test_input($_POST['email']);
+	$mobile = test_input($_POST['mobile']);
 
-// 	$result = $conn->query("UPDATE `users` SET `username` = '$username', `email` = '$email', `mobile` = '$mobile' WHERE `id` = '$id'");
-	
-// 	if($result){
-// 		$res['message'] = "User updated successfully";
-// 	} else{
-// 		$res['error'] = true;
-// 		$res['message'] = "Could not update user";
-// 	}
+	$result = $conn->query("UPDATE `users` SET `username` = '$username', `email` = '$email', `mobile` = '$mobile' WHERE `id` = '$id'");
 
-// }
+	if($result){
+		$res['message'] = "User updated successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not update user";
+	}
 
-// if($action == 'delete'){
-// 	$id = $_POST['id'];
-// 	$result = $conn->query("DELETE FROM `users` WHERE `id` = '$id'");
+}
 
-// 	if($result){
-// 		$res['message'] = "User deleted successfully";
-// 	} else{
-// 		$res['error'] = true;
-// 		$res['message'] = "Could not delete user";
-// 	}
+if($action == 'delete'){
+	$id = test_input($_POST['id']);
+	$result = $conn->query("DELETE FROM `users` WHERE `id` = '$id'");
 
-// }
+	if($result){
+		$res['message'] = "User deleted successfully";
+	} else{
+		$res['error'] = true;
+		$res['message'] = "Could not delete user";
+	}
+
+}
 
 $conn->close();
 header("Content-type: application/json");
 echo json_encode($res);
+
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
 die();
 
 ?>
