f5928cb4d65bba2961a031d26a8231dd6cc34c29
Abir <abiruzzaman.molla@gmail.com>
2022-08-31 17:49:08 +0600

    feat: show users data in table

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ var app = new Vue({
 		showingDeleteModal: false,
 		errorMessage: "",
 		successMessage: "",
-		users: []
+		users: [],
 	},
 
 	mounted: function(){
 		console.log("mounted");
+		this.getAllUsers();
 	},
 
 	methods: {
-
+		getAllUsers: function () {
+			axios.get("http://localhost/vuephp/api.php?action=read")
+				.then(function (response) {
+					// console.log(response);
+					if (response.data.error) {
+						app.errorMessage = response.data.error;
+					} else {
+						app.users = response.data.users;
+					}
+				});
+		}
 	}
 });
